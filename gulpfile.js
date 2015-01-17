024bad0795cfb314ed00e87ca2228b88b62e25ae
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-01-17 13:56:19 +0000

    initial

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var bower = require('bower');
+var concat = require('gulp-concat');
+var sass = require('gulp-sass');
+var minifyCss = require('gulp-minify-css');
+var rename = require('gulp-rename');
+var sh = require('shelljs');
+var inject = require('gulp-inject');
+
+var angularFilesort = require('gulp-angular-filesort');
+var paths = {
+    sass: ['./scss/**/*.scss'],
+    js: ['./www/js/**/*.js']
+
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function(done) {
+    gulp.src('./scss/ionic.app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({
+            extname: '.min.css'
+        }))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
+});
+
+
+gulp.task('injectJS', function() {
+    var target = gulp.src('./www/index.html');
+
+    return target
+        .pipe(inject(
+            gulp.src(paths.js, {})
+            .pipe(angularFilesort()), {
+                ignorePath: '/www/',
+                addRootSlash: false
+
+            }))
+        .pipe(gulp.dest('./www'))
+
+
+});
+
+gulp.task('watch', function() {
+    gulp.watch(paths.sass, ['sass']);
+});
+
+gulp.task('install', ['git-check'], function() {
+    return bower.commands.install()
+        .on('log', function(data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
+});
+
+gulp.task('git-check', function(done) {
+    if (!sh.which('git')) {
+        console.log(
+            '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+            '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+        );
+        process.exit(1);
+    }
+    done();
+});
