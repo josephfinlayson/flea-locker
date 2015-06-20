45364462613c1b6873a72d3145ff76b6f53d6d8b
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-06-20 14:27:54 +0200

    remove cruft, update ionic

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,54 +1,6 @@
 angular.module('starter.controllers', [])
 
     .controller('DashCtrl', function ($scope) {
-        var hk = window.plugins.healthkit;
-
-        function checkAvailable(func) {
-            hk.available(func)
-        }
-
-        function onError() {
-            console.log(arguments)
-            alert("err")
-        }
-
-        function onSuccess() {
-            console.log(arguments)
-            alert("succ")
-        }
-
-        //request weight
-        hk.requestAuthorization(
-            {
-                'readTypes': ['HKQuantityTypeIdentifierBodyMass'],
-                'writeTypes': ['HKQuantityTypeIdentifierBodyMass']
-            },
-            onError,
-            onSuccess
-        )
-
-
-        $scope.weight = {}
-
-        function convertToKilo(kilos) {
-            return kilos;
-        }
-
-        $scope.weight.submit = function (weight) {
-
-            hk.saveWeight(
-                {
-                    'unit': 'kg', // g | kg | oz | lb | st
-                    'amount': convertToKilo(kilos),
-                    'date': new Date() // optional, default is new Date()
-                },
-                onSuccess,
-                onError
-            );
-            weight.input="Submitted";
-        }
-
-
 
     })
 
