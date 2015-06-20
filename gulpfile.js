3028241e5c576cecdd79748a8ef268c3c191b406
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-06-20 15:22:27 +0200

    improving build

--- gulpfile.js
+++ gulpfile.js
@@ -8,17 +8,22 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 var inject = require('gulp-inject');
 
+
+var gulp = require('gulp');
+var browserSync = require('browser-sync').create();
+
+
 var angularFilesort = require('gulp-angular-filesort');
 var paths = {
     sass: ['./scss/**/*.scss'],
-    js: ['./www/js/**/*.js']
-
+    js: ['./www/js/**/*.js'],
+    css: ['./www/css/**/*.css']
 };
 
 gulp.task('default', ['sass']);
 
-gulp.task('sass', function(done) {
-    gulp.src('./scss/ionic.app.scss')
+gulp.task('sass', function (done) {
+    return    gulp.src('./scss/ionic.app.scss')
         .pipe(sass())
         .pipe(gulp.dest('./www/css/'))
         .pipe(minifyCss({
@@ -28,38 +33,47 @@ gulp.task('sass', function(done) {
             extname: '.min.css'
         }))
         .pipe(gulp.dest('./www/css/'))
-        .on('end', done);
+        .pipe(browserSync.stream());
 });
 
 
-gulp.task('injectJS', function() {
+gulp.task('injectJS', function () {
     var target = gulp.src('./www/index.html');
 
     return target
         .pipe(inject(
             gulp.src(paths.js, {})
-            .pipe(angularFilesort()), {
+                .pipe(angularFilesort()), {
                 ignorePath: '/www/',
                 addRootSlash: false
 
             }))
         .pipe(gulp.dest('./www'))
 
-
 });
 
-gulp.task('watch', function() {
-    gulp.watch(paths.sass, ['sass']);
-});
+gulp.task('watch', function () {
+        browserSync.init({
+            server: {
+                baseDir: "./www"
+            }
+        });
 
-gulp.task('install', ['git-check'], function() {
+        gulp.watch(paths.js).on('change', browserSync.reload)
+        gulp.watch(paths.sass, ['sass'])
+        gulp.watch(paths.css).on('change', browserSync.reload)
+        gulp.watch("www/index.html").on('change', browserSync.reload);
+    }
+);
+
+gulp.task('install', ['git-check'], function () {
     return bower.commands.install()
-        .on('log', function(data) {
+        .on('log', function (data) {
             gutil.log('bower', gutil.colors.cyan(data.id), data.message);
         });
 });
 
-gulp.task('git-check', function(done) {
+gulp.task('git-check', function (done) {
     if (!sh.which('git')) {
         console.log(
             '  ' + gutil.colors.red('Git is not installed.'),
