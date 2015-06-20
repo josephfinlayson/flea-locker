2579d788a46463f031978916cdefb4e2834c1a44
Georg Nordhausen <onionblossom@gmail.com>
2015-06-20 19:07:25 +0200

    * added photo taking ability

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,49 @@
 angular.module('starter.controllers', [])
 
-    .controller('buyCtrl', function ($scope) {
+.config(function($compileProvider) {
+  $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
+})
 
-    })
 
-    .controller('sellCtrl', function ($scope) {
 
-    })
-    .controller('accountCtrl', function ($scope) {
 
-    })
+
+.controller('buyCtrl', function($scope) {
+  //takePicture();
+})
+
+
+.controller('sellCtrl', function($scope, Camera) {
+  //takePicture();
+  $scope.data = { "ImageURI" :  "Select Image" };
+
+  $scope.getPhoto = function() {
+    console.log('Getting camera');
+    Camera.getPicture().then(function(imageURI) {
+      $scope.data.ImageURI =  imageURI;
+
+
+      alert(imageURI);
+      $scope.lastPhoto = imageURI;
+    //  $scope.upload();
+    }, function(err) {
+      console.err(err);
+    }, {
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false,
+    //  destinationType: Camera.DestinationType.DATA_URL
+    });
+  };
+
+
+
+
+
+})
+
+
+.controller('accountCtrl', function($scope) {
+
+})
--- www/js/services.js
+++ www/js/services.js
@@ -1,5 +1,27 @@
 angular.module('starter.services', [])
 
+
+// Camera stuff
+.factory('Camera', ['$q', function($q) {
+
+  return {
+    getPicture: function(options) {
+      var q = $q.defer();
+
+      navigator.camera.getPicture(function(result) {
+        // Do any magic you need
+        q.resolve(result);
+      }, function(err) {
+        q.reject(err);
+      }, options);
+
+      return q.promise;
+    }
+  }
+}])
+
+
+
 .factory('Chats', function() {
   // Might use a resource here that returns a JSON array
 
