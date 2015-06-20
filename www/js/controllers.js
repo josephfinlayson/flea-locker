caf64fc9b084a3c027a738a8399be894dcbe0ed5
elektrowolle <internetbewohner@gmail.com>
2015-06-20 20:36:43 +0200

    New success tab

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -8,21 +8,6 @@ angular.module('starter.controllers', [])
   $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|ftp|mailto|file|tel):/);
 })
 
-    .controller('sellCtrl', function ($scope) {
+    .controller('successCtrl', function ($scope) {
 
     })
-    .controller('accountCtrl', function ($scope) {
-    })
-
-
-
-.controller('buyCtrl', function($scope) {
-  //takePicture();
-})
-
-
-
-
-.controller('accountCtrl', function($scope) {
-
-})
