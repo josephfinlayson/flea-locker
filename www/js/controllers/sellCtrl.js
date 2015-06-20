97869b0bc98f11f875129b5f6f3d4311cea980d0
Joseph Finlayson <joseph.finlayson@gmail.com>
2015-06-20 20:24:20 +0200

    add places to map

--- www/js/controllers/sellCtrl.js
+++ www/js/controllers/sellCtrl.js
@@ -1,17 +1,52 @@
 angular.module('starter.controllers')
-    .controller('sellCtrl', function ($scope, getCurrentGeo, $compile) {
+    .controller('sellCtrl', function ($scope, getCurrentGeo, $compile, getLocalLockers) {
+        var myLatlng;
+        var globalMap;
+
         $scope.$on('$ionicView.enter', function () {
-            initialize()
+            getCurrentGeo
+                .then(initialize)
+                .then(doPlacesSearch)
+                .then(placeMarkersOnMap)
         });
 
-        getCurrentGeo.then(function (a) {
-            console.log(a)
-        })
+        function doPlacesSearch(map) {
+            return getLocalLockers(map, myLatlng)
+        }
 
-        function initialize() {
+        function placeMarkersOnMap(places) {
+            //console.log(places)
+            createMarkers(places);
+        }
+
+        function createMarkers(places) {
+
+            for (var i = 0, place; place = places[i]; i++) {
+                var image = {
+                    url: place.icon,
+                    size: new google.maps.Size(71, 71),
+                    origin: new google.maps.Point(0, 0),
+                    anchor: new google.maps.Point(17, 34),
+                    scaledSize: new google.maps.Size(25, 25)
+                };
+
+                var marker = new google.maps.Marker({
+                    map: globalMap,
+                    icon: image,
+                    title: place.name,
+                    position: place.geometry.location
+                });
+
+                marker.setMap(globalMap)
+            }
+        }
+
+
+        function initialize(latLong) {
             console.log("initialising")
 
-            var myLatlng = new google.maps.LatLng(43.07493, -89.381388);
+            myLatlng = new google.maps.LatLng(latLong.coords.latitude, latLong.coords.longitude);
+
             var mapOptions = {
                 center: myLatlng,
                 zoom: 16,
@@ -38,8 +73,9 @@ angular.module('starter.controllers')
             google.maps.event.addListener(marker, 'click', function () {
                 infowindow.open(map, marker);
             });
-
+            globalMap = map
             $scope.map = map;
+            return map;
         }
 
         $scope.centerOnMe = function () {
@@ -58,11 +94,28 @@ angular.module('starter.controllers')
             }, function (error) {
                 alert('Unable to get location: ' + error.message);
             });
+
         };
 
         $scope.clickTest = function () {
             alert('Example of infowindow with ng-click')
         };
 
+    }).service('getLocalLockers', function ($q) {
+        return function (map, latLong) {
+            var request = {
+                location: latLong,
+                rankBy: google.maps.places.RankBy.DISTANCE,
+                radius: 11200,
+                query: 'library'
+            }
+
+            return $q(function (res, rej) {
+                var service = new google.maps.places.PlacesService(map);
+                service.textSearch(request, res)
+            })
+
+        }
     })
+
 
