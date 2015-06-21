1f83e36a34db574deac4c3cd098229864f9443b9
elektrowolle <internetbewohner@gmail.com>
2015-06-21 10:04:52 +0200

    just fixed

--- www/js/controllers/successCtl.js
+++ www/js/controllers/successCtl.js
@@ -2,7 +2,7 @@
  * Created by benjaminskirlo1 on 20.06.15.
  */
 angular.module('starter.controllers')
-    .controller('successCtrl', function ($scope, $stateParams, successItem) {
+    .controller('successCtrl', function ($scope, $stateParams) {
         $scope.item = $stateParams.item;
         /*myLatlng = new google.maps.LatLng(latLong.coords.latitude, latLong.coords.longitude);
          /*/
